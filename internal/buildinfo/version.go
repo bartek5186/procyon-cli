e22c7b59fe2e30330a26d2f3c58a285e3a09ceaf
Bartek <bartek5186@gmail.com>
2026-07-21 12:19:52 +0200

    release procyon-cli v0.7.5

--- internal/buildinfo/version.go
+++ internal/buildinfo/version.go
@@ -1,7 +1,7 @@
 package buildinfo
 
 const (
-	CLIVersion      = "0.7.4"
-	TemplateVersion = "v0.7.2"
-	CoreVersion     = "v0.7.2"
+	CLIVersion      = "0.7.5"
+	TemplateVersion = "v0.7.3"
+	CoreVersion     = "v0.7.3"
 )
